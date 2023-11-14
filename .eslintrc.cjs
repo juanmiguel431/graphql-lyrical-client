f1540bcdc449833c2dbf36c82f1915d20f21a8ee
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-13 23:45:05 -0400

    ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,43 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  settings: {
+    'react': {
+      'version': 'detect'
+    }
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['type' | 'interface'],
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
